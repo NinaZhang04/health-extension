68fa7969cba2135706b2fe994006524272439f31
Adrien Chen <cyb976933509@gmail.com>
2022-08-14 05:46:38 -0400

    Refactored popup.js

--- popup.js
+++ popup.js
@@ -1,117 +1,62 @@
 // By pressing this button it should read all the fields
 const ele = document.getElementById("btn")
 ele.addEventListener("click", () => {
-  chrome.runtime.sendMessage({ time: "1", repeat: 30}, function (response) {
-    console.log(response);
-  });
-  chrome.runtime.sendMessage({ time: "2", repeat: 60}, function (response) {
-    console.log(response);
-  });
+  sendMsg('1',30);
+  sendMsg('2',60);
+
   // Activation button for the Pomodoro method:
-  chrome.runtime.sendMessage({ time: "3", repeat: 10}, function (response) {
-    console.log(response);
-  }); 
-  chrome.runtime.sendMessage({ time: "4", repeat: 23}, function (response) {
-    console.log(response);
-  }); 
+  sendMsg('3',10);
+  sendMsg('4',23);
 });
 
 
 // Water break set
 const water_alarm = document.getElementById("water_break");
-water_alarm.addEventListener("click", () => {
-  let waterTime = parseFloat(document.getElementById("waterNum").value);
-  chrome.runtime.sendMessage({ time: "1" , repeat: waterTime}, function (response) {
-    console.log(response);
-  }, );
+water_alarm.addEventListener("click", () => {sendMsg('1',parseFloat(document.getElementById("waterNum").value));
+clearInput("waterNum");
 });
-
 // Screen break set
 const screen_alarm = document.getElementById("screen_break");
-screen_alarm.addEventListener("click", () => {
-  let screenTime = parseFloat(document.getElementById("screenNum").value);
-  chrome.runtime.sendMessage({ time: "2" , repeat: screenTime}, function (response) {
-    console.log(response);
-  });
+screen_alarm.addEventListener("click", () => {sendMsg('2',parseFloat(document.getElementById("screenNum").value));
+clearInput("screenNum")
 });
 
 // Activation for the Pomodoro method:
 const pomodoroButton = document.getElementById("pomodoro_timer");
-pomodoroButton.addEventListener("click", () => {
-  chrome.runtime.sendMessage({ time: "3", repeat: 1}, function (response) {
-    console.log(response);
-  }); 
-});
+pomodoroButton.addEventListener("click", () => {sendMsg('3',1)});
 
 // Sleep reminder
 const sleepButton = document.getElementById("sleep_reminder");
-sleepButton.addEventListener("click", () => {
-  let sleepNum = parseFloat(document.getElementById("sleepNum").value);
-  chrome.runtime.sendMessage({ time: "4" ,repeat: sleepNum}, function (response) {
-    console.log(response);
-  });
+sleepButton.addEventListener("click", () => {sendMsg('4',parseFloat(document.getElementById("sleepNum").value));
+clearInput("sleepNum")
 });
 
 //Stop water alarms
 const stopWater = document.getElementById("stopWater");
-stopWater.addEventListener("click", () => {
-  chrome.runtime.sendMessage({ time: "5" ,repeat: 0}, function (response) {
-  });
-});
+stopWater.addEventListener("click", () => {sendMsg('5',0)});
 
 //Stop screen alarms
 const stopScreen = document.getElementById("stopScreen");
-stopScreen.addEventListener("click", () => {
-  chrome.runtime.sendMessage({ time: "6" ,repeat: 0}, function (response) {
-  });
-});
+stopScreen.addEventListener("click", () => {sendMsg('6',0)});
 
 //Stop pomodoro alarms
 const stopPomodoro = document.getElementById("stopPomodoro");
-stopPomodoro.addEventListener("click", () => {
-  chrome.runtime.sendMessage({ time: "7" ,repeat: 0}, function (response) {
-  });
-});
-
+stopPomodoro.addEventListener("click", () => {sendMsg('7',0)});
 
 //Stop sleep alarms
 const stopSleepReminder = document.getElementById("stopSleepReminder");
-stopSleepReminder.addEventListener("click", () => {
-  chrome.runtime.sendMessage({ time: "8" ,repeat: 0}, function (response) {
-  });
-});
+stopSleepReminder.addEventListener("click", () => {sendMsg('8',0)});
 
 //Stop ALL ALARMS
 const stopAllReminders = document.getElementById("stopAllReminders");
-stopAllReminders.addEventListener("click", () => {
-  chrome.runtime.sendMessage({ time: "9" ,repeat: 0}, function (response) {
+stopAllReminders.addEventListener("click", () => {sendMsg('9',0);});
+
+//helper
+function sendMsg(t, rep){
+  chrome.runtime.sendMessage({time: t, repeat: rep}, function (response) {
   });
-});
+};
 
-
-/*
-// Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
-
-// The body of this function will be execuetd as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
+function clearInput(name){
+  document.getElementById(name).value = "";
 }
-*/
