c0d0db14d48aa25b710166cbefe04dd3c6f5273f
Adrien Chen <cyb976933509@gmail.com>
2022-08-14 05:46:24 -0400

    Refactored background.js

--- background.js
+++ background.js
@@ -1,176 +1,95 @@
-let color = '#3aa757';
-/*
-//newly added
-//try to keep background process persistent
-let lifeline;
-keepAlive();
-*/
 
-/*
-//newly added
-chrome.runtime.onConnect.addListener(port => {
-  if (port.name === 'keepAlive') {
-    lifeline = port;
-    setTimeout(keepAliveForced, 295e3); // 5 minutes minus 5 seconds
-    port.onDisconnect.addListener(keepAliveForced);
-  }
-});
+var dict = {
+  1: "drink_water",
+  2: "screen_time_check",
+  3: "pomodoro_method",
+  4: "sleep-reminder",
+  5: "drink_water",
+  6: "screen_time_check",
+  7: "pomodoro_method",
+  8: "sleep-reminder",
+  9: "clearAll"
+};
 
-//newly added
-function keepAliveForced() {
-  lifeline?.disconnect();
-  lifeline = null;
-  keepAlive();
-}
-*/
+var activityList = Array(6).fill("inactive");
 
-
-let water_alarm;
+chrome.action.setBadgeText({text: 'OFF'});
 
 chrome.alarms.onAlarm.addListener(
 
   function(alarm){
     //determine which alarm is set off
-
     //log to keep track which alarm is set off
-    console.log(alarm.name);
 
-    if (alarm.name === "drink_water"){
-      chrome.notifications.create(
-        {
-          type: "basic",
-          iconUrl: "/images/get_started128.png",
-          title: "Stay Hydrated!",
-          message: "Remeber to drink your water :)",
-          silent: false
-        },
-        () => { }
-      )
+    var notifTitle; 
+    var notifMsg;
+
+    switch(alarm.name){
+      case "drink_water":
+        notifTitle = "Stay Hydrated!"
+        notifMsg = "Remeber to drink your water :)"
+        break;
+
+      case "screen_time_check":
+        notifTitle = "Screen time reminder"
+        notifMsg = "You have been using your computer for a while!"
+        break;
+
+      case "pomodoro_method":
+        notifTitle = "Pomodoro Method: take your 5 minute break now!"
+        notifMsg = "You have worked for 25 minutes continously, now take a 5 minute break and get your mind back in focus!"
+        break;
+
+      case "sleep-reminder":
+        notifTitle = "Time to sleep!"
+        notifMsg = "Right now it is the best for your health to go to sleep"
+        break;
     }
 
-    else if (alarm.name === "screen_time_check"){
-      chrome.notifications.create(
-        {
-          type: "basic",
-          iconUrl: "/images/get_started128.png",
-          title: "Screen time reminder",
-          message: "You have been using your computer for a while!",
-          silent: false
-        },
-        () => { }
-      )
-    }
-
-    else if (alarm.name === "pomodoro_method"){
-      chrome.notifications.create(
-        {
-          // Alarm pop-up content is noted down below:
-          type: "basic",
-          iconUrl: "/images/get_started128.png",
-          title: "Pomodoro Method: take your 5 minute break now!",
-          message: "You have worked for 25 minutes continously, now take a 5 minute break and get your mind back in focus!",
-          silent: false
-        },
-        () => { }
-      )
-    }
-
-    else if(alarm.name === "sleep-reminder"){
-      chrome.notifications.create(
-        {
-          // Alarm pop-up content is noted down below:
-          type: "basic",
-          iconUrl: "/images/get_started128.png",
-          title: "Time to sleep!",
-          message: "Right now it is the best for your health to go to sleep",
-          silent: false
-        },
-        () => { }
-      )
-    }
+    chrome.notifications.create(
+      {
+        type :"basic",
+        iconUrl: "/images/balance128.png",
+        title: notifTitle,
+        message: notifMsg,
+        silent: false
+      },
+      () => {}
+    )
+    console.log("current alarm: " + alarm.name);
   }
 )
 
 
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
-    //console.log(request);
-    if (request.time == 1){
-      createAlarm(1, request.repeat);
-    } 
-    else if(request.time == 2){
-      createAlarm(2, request.repeat);
-    } 
-    else if (request.time == 3) {
-      createAlarm(3, 1); // note: the second parameter wouldn't matter for the specified function.
-    } 
-    else if (request.time == 4) {
-      createAlarm(4, request.repeat); // note: the second parameter wouldn't matter for the specified function.
+    if (request.time <5){
+      createAlarm(request.time, request.repeat);
     }
-    else if(request.time  == 5){
-      console.log(5);
-      stopWaterAlarm();
+    else if (request.time >= 5){
+      stopAlarm(request.time);
     }
-    else if(request.time  == 6){
-      console.log(6);
-      stopScreenTimeAlarm();
-    }
-    else if(request.time  == 7){
-      console.log(7);
-      stopPomodoroAlarm();
-    }
-    else if(request.time  == 8){
-      console.log(8);
-      stopSleepAlarm();
-    }
-    else if(request.time  == 9){
-      console.log(9);
-      stopAllAlarm();
-    }
-
   }
 );
 
-//type 1 is water reminder, type 2 is screen time reminder
+
 function createAlarm(type, num){
-  if(type == 1){
+  chrome.action.setBadgeText({text: 'ON'});
+
+  //pomodoro
+  if (type == 3){
     chrome.alarms.create(
-      "drink_water",
+      dict[type],
       {
-        //Length of time in minutes after which the onAlarm event should fire.
-        delayInMinutes: num,
-  
-        //If set, the onAlarm event should fire every periodInMinutes minutes after the initial event specified
-        periodInMinutes: num
-      }
-    )
-  }
-  else if(type == 2){
-    chrome.alarms.create(
-      "screen_time_check",
-      {
-        //Length of time in minutes after which the onAlarm event should fire.
-        delayInMinutes: num,
-  
-        //If set, the onAlarm event should fire every periodInMinutes minutes after the initial event specified
-        periodInMinutes: num
-      }
-    )
-  } else if (type == 3) {
-    chrome.alarms.create(
-      "pomodoro_method",
-      {
-        // Timespan is noted down below:
         delayInMinutes: 25,
-        periodInMinutes: 30,
+        periodInMinutes: 30
       }
     )
   }
   else if (type == 4){
     //sleep time reminder
     console.log(Date());
-    
-    
+
     //this does the timezone offset
     const d = new Date();
     let diff = d.getTimezoneOffset()*1000*60;
@@ -187,11 +106,8 @@ function createAlarm(type, num){
     if(addedTime <= 0){
       addedTime += 1000*3600*24;
     }
-    else{
-      //
-    }
 
-  console.log(Date.now() + addedTime);
+    console.log(Date.now() + addedTime);
     chrome.alarms.create(
       "sleep-reminder",
       {
@@ -203,27 +119,29 @@ function createAlarm(type, num){
       }
     )
   }
-
+  else {
+    chrome.alarms.create(
+      dict[type],
+      {
+        delayInMinutes: num,
+        periodInMinutes: num
+      }
+    )
+  }
+  activityList[type-1] = "active";
 }
 
+function stopAlarm(type){
+  if (dict[type] == "clearAll"){
+    chrome.alarms.clearAll();
+    activityList = Array(6).fill("inactive");
+  }
+  else {
+    chrome.alarms.clear(dict[type]);
+    activityList[(type-5)] = "inactive";
+  }
+  if (activityList.findIndex((value)=>value === "active") == -1){
 
-function stopWaterAlarm(){
-  chrome.alarms.clear("drink_water");
+    chrome.action.setBadgeText({text: 'OFF'});
+  }
 }
-
-function stopScreenTimeAlarm(){
-  chrome.alarms.clear("screen_time_check");
-}
-
-function stopPomodoroAlarm(){
-  chrome.alarms.clear("pomodoro_method");
-}
-
-function stopSleepAlarm(){
-  chrome.alarms.clear("sleep-reminder");
-}
-
-function stopAllAlarm(){
-  chrome.alarms.clearAll();
-}
-  
